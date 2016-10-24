1506c8b678e84a53e28c6e64ca81a97664bfd05d
dnyaneshmali <dnyanmali@gmail.com>
2016-10-24 14:31:34 +0530

    header footer

---
+++ application/views/header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">        <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'css/materialize.min.css' ?> " />
+
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $m_title ?></title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'css/font-awesome.min.css' ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'css/owl.carousel.css' ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'css/style.css' ?>" />
+
+    </head>
+    <body>
+        <div class="header-top grey darken-3 ">
+
+            <div class="row">
+                <div class="col s12 ">
+                    <ul class="top-nav">
+                        <li><a href="#">INFO@ETHIOEDU.COM</a></li>
+
+
+                        <li><a href="#">CONTACT/HOTLINE</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/About_us' ?>">ABOUT US</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/contact' ?>">CONTACT US</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/csregister' ?>">REGISTER </a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/login' ?>">LOGIN</a></li>
+
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+        <div class="header-top1 grey darken-4 ">
+
+            <div class="row">
+                <div class="col s3 ">
+                    <img class="logo_bar" src="<?php echo base_url() . 'images/logo.png' ?>">
+                </div>
+                <div class="col s9 ">
+                    <div class="upper_slider">
+                        <div class="item"><img src="<?php echo base_url() . 'images/slider_upper/banner_Horizontal.jpg' ?>"></div>
+                        <div class="item"><img src="<?php echo base_url() . 'images/slider_upper/benner_Horizontal1.jpg' ?>"></div>
+                        <div class="item"><img src="<?php echo base_url() . 'images/slider_upper/banner_Horizontal2.png' ?>"></div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="navbar-fix">
+            <nav class="grey darken-4" role="navigation">
+                <div class="nav-wrapper container">
+                    <ul id="dropdown1" class="dropdown-content dropdown-menu">
+                        <li><a href="<?php echo base_url() . 'index.php/news' ?>">Latest News</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/reportnews' ?>">Report News</a></li>
+                        <li class="divider"></li>
+                        <li><a href="<?php echo base_url() . 'index.php/gist' ?>">Latest Gist & Gossip</a></li>
+                    </ul>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a class="active" href="<?php echo base_url() . 'index.php/home' ?>">HOME</a></li>
+
+                        <li><a class="dropdown-button home-btn" href="#!" data-hover="true" data-activates="dropdown1">LATEST NEWS </a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/schools' ?>">SCHOOLS</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/course' ?>">COURSES</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/vactender' ?>">VACANCY/TENDER</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/connectme' ?>">CONNECT TO ME</a></li>
+                        <li><a href="#">SERVICE</a></li>
+
+                    </ul>
+
+                    <ul id="nav-mobile" class="side-nav mobile-nav">
+                        <li><a class="active" href="#">HOME</a></li>
+
+                        <li><a class="dropdown-button home-btn" href="#!" >LATEST NEWS</a></li>
+                        <li><a href="#">SCHOOLS</a></li>
+                        <li><a href="#">COURSES</a></li>
+                        <li><a href="#">VACANCY/TENDER</a></li>
+                        <li><a href="#">CONNECT TO ME</a></li>
+                        <li><a href="#">SERVICE</a></li>
+                    </ul>
+                    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+                </div>
+            </nav>
+        </div>
