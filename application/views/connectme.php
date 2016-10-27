b3b8391b4569a9aa41596c83d7fb1292e8c918cf
harshalghule <harshal.ghule@softinfology.com>
2016-10-27 18:31:34 +0530

    connect me merge conflict

--- application/views/connectme.php
+++ application/views/connectme.php
@@ -152,7 +152,6 @@
         </div>
         <?php $this->load->view('footer'); ?>
 		
-		
 		<!-- <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css"> -->
 <!-- <script type="text/javascript" src="../js/bootstrap.min.js"></script> -->
 
