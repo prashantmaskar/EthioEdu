1dbbb32561cc6ce3ac1b7f376d4a2e2a52e13638
Santosh <santosh.bhosale123@gmail.com>
2016-10-27 01:44:07 -0700

    validation

--- application/views/project_topics.php
+++ application/views/project_topics.php
@@ -22,17 +22,17 @@
                         </div>
                         <div class="col s12 m12 ">
                             <div class="news_add">
-                                <form class="col m12">
-                                    <div class="input-field col s12">
-                                        <input id="Caption" type="text" class="validate">
-                                        <label for="Caption">Project Name</label>
-                                    </div>
-                                    <div class="input-field col s12">
-                                        <input id="c_name" type="text" class="cname">
-                                        <label for="c_name">Course Name</label>
-                                    </div>
-                                    <div class="input-field col s12">
-                                        <select >
+                               <form id="project_topic" action="#" method="post" class="col m12">
+                                     <div class="form-group input-field col s12">
+                                                        <input id="Project_name" name="Project_name" type="text" class="validate">
+                                                        <label for="Project_name">Project Name</label>
+                                                    </div>
+                                     <div class="form-group input-field col s12">
+                                                        <input id="course_name" name="course_name" type="text" class="validate">
+                                                        <label for="course_name">Course Name</label>
+                                                    </div>
+                                    <div class="form-group input-field col s12">
+                                       <select class="form-control browser-default" name="catagory">
                                             <option value="" disabled selected>Category</option>
                                             <option value="1">Admission</option>
                                             <option value="2">Departmental</option>
@@ -47,36 +47,21 @@
                                             <option value="8">Scholarship</option>
                                             <option value="8">Other</option>
                                         </select>
-                                        <label>Category</label>
-                                    </div>
-                                    <div class="input-field col s12">
-                                        <select >
-                                            <option value="" disabled selected>File Format</option>
-                                            <option value="1">Admission</option>
-                                            <option value="2">Departmental</option>
-                                            <option value="3">Entertainments</option>
-                                            <option value="4">Events</option>
-                                            <option value="5">General</option>
-                                            <option value="6">Part Time</option>
-                                            <option value="7">Full Time</option>
-                                            <option value="8">Sport</option>
-                                            <option value="9">Pre Degree</option>
-                                            <option value="10">Post Degree</option>
-                                            <option value="8">Scholarship</option>
-                                            <option value="8">Other</option>
-                                        </select>
-                                        <label>File Format</label>
-                                    </div>
-                                    <div class="file-field input-field col s12">
-                                        <div class="btn">
-                                            <span>Add File</span>
-                                            <input type="file" multiple>
-                                        </div>
-                                        <div class="file-path-wrapper">
-                                            <input class="file-path validate" type="text" placeholder="Add Zip Project File">
-                                        </div>
-                                    </div>
-   
+                                 </div>
+                                   
+                                    <div class="form-group input-field col s12">
+                                                        <select class="form-control browser-default" name="fileformat">
+                                                            <option  value="File Format">File Format</option>
+                                                            <option value="Word">Word</option>
+                                                            <option value="Pdf">Pdf</option>
+                                                            <option value="Rtf">Rtf</option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="form-group file-field input-field col s12">
+                                                        <div class="btn" name="upload1">
+                                                            <span>Attach File</span>
+                                                            <input type="file" multiple>
+                                                        </div>
                                     <div class="col s5 offset-s5 margin-b-10">
                                         <button class="btn waves-effect waves-light" type="submit" name="action">Add
                                            
@@ -104,3 +89,75 @@
             </div>
         </div>
         <?php $this->load->view('footer'); ?>
+
+
+        <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="../css/bootstrapValidator.css">
+<script type="text/javascript" src="../js/bootstrapValidator.js"></script>
+<script type="text/javascript" src="../js/admin/admin-project-topic.js"></script>
+
+
+<script>
+   $(document).ready(function() {
+
+    $('#project_topic').bootstrapValidator({
+        /*feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },*/
+        fields: {
+            Project_name: {
+                validators: {
+                    notEmpty: {
+                        message: ' please enter the name of project!!!..'
+                    }
+                }
+            },
+              course_name: {
+                validators: {
+                    notEmpty: {
+                        message: ' please enter cource name !!!..'
+                    }
+                }
+            },
+ 
+
+                  catagory: {
+                validators: {
+                    notEmpty: {
+                        message: ' please select catagory!!!..'
+                    }
+                }
+            },
+ 
+              
+                  fileformat: {
+                validators: {
+                    notEmpty: {
+                        message: ' please select file format!!!..'
+                    }
+                }
+            },
+ 
+               
+                  upload1: {
+                validators: {
+                    notEmpty: {
+                        message: ' you must upload file!!!..'
+                    }
+                }
+            },
+ 
+
+
+
+
+        }
+    });
+});
+
+
+
+</script>
