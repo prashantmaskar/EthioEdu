15f195b838a62add5e7b4a342d6fe1f2837e9017
harshalghule <harshal.ghule@softinfology.com>
2016-11-04 15:31:11 +0530

    advertising updated design

--- application/views/Advertising.php
+++ application/views/Advertising.php
@@ -22,20 +22,20 @@
                         </div>
                         <div class="col s12 m12 ">
                             <div class="advertise_info">
-                                <p>Ethioedu Limited runs www.ethioedu.com, Ethiopia's No1 Students' and Educational Resource Website with the highest traffic and pageviews from youths, students and prospective students across the country. With this status, we command the Ethiopia Educational Sphere. Students and youths across the country use our platform to interact and gain relevant information and resources that impact positively on their educational pursuits, hence the reason why they come over and over again.</p>
+                                <p class="black-text">Ethioedu Limited runs www.ethioedu.com, Ethiopia's No1 Students' and Educational Resource Website with the highest traffic and pageviews from youths, students and prospective students across the country. With this status, we command the Ethiopia Educational Sphere. Students and youths across the country use our platform to interact and gain relevant information and resources that impact positively on their educational pursuits, hence the reason why they come over and over again.</p>
                                 <h4 class="ad_sbtitle">Let's start with our Site Statistics:</h4>
-                                <p>Our website statistics is based on PURE STUDENTS, that are mostly seeking admission or already in the higher institution.</p>
+                                <p class="black-text">Our website statistics is based on PURE STUDENTS, that are mostly seeking admission or already in the higher institution.</p>
                                 <ul>
                                     <li> 8.5 Million page views per month[Currently]</li>
                                     <li> 2.3 Million Sessions Per Month [Average]</li>
                                 </ul>
                                 <h4 class="ad_sbtitle red-text bo_bor padd_b5">Let's get started... Let's know what you need. Write us using the form below</h4>
                                 <form id="advertiseform" class="col m12">
-                                    <div class=" form-group" input-field col s12">
+                                    <div class=" form-group input-field col s12">
                                         <input id="Caption" name="Caption" type="text" class="validate">
                                         <label for="Caption">Name</label>
                                     </div>
-                                <div class="form-group" input-field col s12">
+                                <div class="form-group input-field col s12">
                                         <input id="Caption" name="email" type="email" class="validate">
                                         <label for="Caption">Email</label>
                                     </div>
@@ -43,7 +43,7 @@
                                         <input id="Caption" name="contact" type="number" class="validate">
                                         <label for="Caption">Contact</label>
                                     </div>
-                                    <div class="input-field col s12">
+                                    <div class="form-group input-field col s12">
                                         <select class="form-control" name="catagory" >
                                             <option value="" disabled selected>Category</option>
                                             <option value="1">Admission</option>
