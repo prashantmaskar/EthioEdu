9149eac7b3c9f31db603d8eb119c69c7f85fd842
Santosh <santosh.bhosale123@gmail.com>
2016-11-09 02:32:39 -0800

    changes for news

--- application/controllers/reportnews.php
+++ application/controllers/reportnews.php
@@ -40,5 +40,68 @@ class reportnews extends CI_Controller {
         );
         $this->load->view('reportnews',$view_params);
     }
+       public function add()
+       {
+            $data=array(
+                'post_title' => $this->input->post('caption'),
+                'post_desc'  => $this->input->post('Description'),
+                'post_category'=>$this->input->post('catagory'),
+                 'post_attachment' =>'',
+                 'post_author'=>  $this->input->post('auther'),
+                   'post_date' => $this->input ->post('date'),
+                  'post_source' => $this->input->post('source_link'),
+                 'post_type'=>  $this->input->post('post_type'),
+                 'user_id'=>''
+        );
+
+                $config['upload_path']          = './uploads/';
+                $config['allowed_types']        = 'gif|jpg|png';
+                $config['max_size']             = 100000;
+                $config['max_width']            = 1024;
+                $config['max_height']           = 768;
+
+                $this->load->library('upload', $config);
+
+                if ( ! $this->upload->do_upload('avatar'))
+                {
+                        $error = array('error' => $this->upload->display_errors());
+
+                        var_dump($error);
+
+                       // $this->load->view('upload_form', $error);
+                }
+                else
+                {
+                        $data1 = array('upload_data' => $this->upload->data());
+
+                        //$db = $this->db->conn_id;
+
+                        
+
+                        $filedata= array(
+                            'file_name' => $data1['upload_data']['file_name'],
+                            'mime_type' => $data1['upload_data']['file_type'],
+                            'blob' => $data1['upload_data']['full_path']
+                        );
+
+                        $data['post_attachment']=$this->init_models->add_attachment($filedata);
+                        $isinserted = $this->init_models->add_anews($data);
+                        
+
+                        //
+                }
+
+               if(isset($isinserted)){
+                    $res=array('success'=>true,"msg"=>'data added successfully');
+                    //$this->load->view('upload_success', $res);
+               }else{
+                    $res=array('success'=>false,"msg"=>'data add failed');
+                    //$this->load->view('upload_success', $res);
+               }
+               var_dump($res);
+
+    }
+
+
 
 }
--- application/models/init_models.php
+++ application/models/init_models.php
@@ -3,61 +3,92 @@
    {  
       function __construct()  
       {  
-			 // Call the Model constructor  
-			 parent::__construct();  
-			 
-			 $this->load->database(); 
+          // Call the Model constructor  
+          parent::__construct();  
+          
+          $this->load->database(); 
       }  
-	  
-      //Select User tables data	  
+     
+      //Select User tables data    
       public function get_user_credentials($username, $password)  
       {  
-			 $query = $this->db->query("SELECT username, password, user_email FROM tbl_users where username='".$username."'  and password = '" . md5($password) . "'");
-			return $query->result_array();
+          //$username ="dnyanesh";
+          $query = $this->db->query("SELECT username, password, user_email FROM tbl_users where username='".$username."'  and password = '" . md5($password) . "'");
+         return $query->row_array();
+
       }
+
       // Insert vacancy data
       public function insertprivacy($data)  
       {  
-				return $this->db->insert('tbl_vacancy', $data);
+            return $this->db->insert('tbl_vacancy', $data);
       }
+
       //Insert course data
       public function insertcourse($data)  
       {  
             return $this->db->insert('tbl_course', $data);
       }
+
+
        
       //Insert School Information data
       public function addschooluser($data)  
       {  
             return $this->db->insert('tbl_users', $data);
       }
+
+
       //Select User tables data    
       public function getueserid()  
       {  
             $query = $this->db->query("SELECT user_id FROM tbl_users ORDER BY user_id DESC LIMIT 1");
             return $query->row_array();
+
       }
+
+
        //Insert School Information data
       public function addschooldata($data)  
       {  
             return $this->db->insert('tbl_school_meta', $data);
       }
+
+
        //Insert User registration data
       public function add_front_user($data)  
       {  
             return $this->db->insert('tbl_users', $data);
       }
+
        //Select User tables data    
       public function getfrontueserid()  
       {  
             $query = $this->db->query("SELECT user_id FROM tbl_users ORDER BY user_id DESC LIMIT 1");
             return $query->row_array();
+
       }
+
         //Insert School Information data
       public function adduserdetails($data)  
       {  
             return $this->db->insert('tbl_user_meta', $data);
       }
-	  
+
+      public function add_attachment($data){
+         $file_data = $this->db->escape_str(file_get_contents($data['blob']));
+         $data['blob']=$file_data;
+         $this->db->insert('tbl_attachments', $data);
+         $insert_id = $this->db->insert_id();
+         return  $insert_id;
+      }
+
+      public function add_anews($data){
+         $this->db->insert('tbl_posts', $data);
+         $insert_id = $this->db->insert_id();
+         return  $insert_id;
+      }
+   
+     
    }  
 ?>  
