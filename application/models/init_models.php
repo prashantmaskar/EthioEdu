a277004e28953fd671b90fd81077fe6cd911eccc
Santosh <santosh.bhosale123@gmail.com>
2016-11-09 01:01:57 -0800

    conflict

--- application/models/init_models.php
+++ application/models/init_models.php
@@ -12,71 +12,52 @@
       //Select User tables data	  
       public function get_user_credentials($username, $password)  
       {  
-          //$username ="dnyanesh";
 			 $query = $this->db->query("SELECT username, password, user_email FROM tbl_users where username='".$username."'  and password = '" . md5($password) . "'");
-			return $query->row_array();
-
+			return $query->result_array();
       }
-
       // Insert vacancy data
       public function insertprivacy($data)  
       {  
 				return $this->db->insert('tbl_vacancy', $data);
       }
-
       //Insert course data
       public function insertcourse($data)  
       {  
             return $this->db->insert('tbl_course', $data);
       }
-
-
        
       //Insert School Information data
       public function addschooluser($data)  
       {  
             return $this->db->insert('tbl_users', $data);
       }
-
-
       //Select User tables data    
       public function getueserid()  
       {  
             $query = $this->db->query("SELECT user_id FROM tbl_users ORDER BY user_id DESC LIMIT 1");
             return $query->row_array();
-
       }
-
-
        //Insert School Information data
       public function addschooldata($data)  
       {  
             return $this->db->insert('tbl_school_meta', $data);
       }
-
-
        //Insert User registration data
       public function add_front_user($data)  
       {  
             return $this->db->insert('tbl_users', $data);
       }
-
        //Select User tables data    
       public function getfrontueserid()  
       {  
             $query = $this->db->query("SELECT user_id FROM tbl_users ORDER BY user_id DESC LIMIT 1");
             return $query->row_array();
-
       }
-
         //Insert School Information data
       public function adduserdetails($data)  
       {  
             return $this->db->insert('tbl_user_meta', $data);
       }
-
-
-
 	  
    }  
 ?>  
