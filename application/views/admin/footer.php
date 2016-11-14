99cb033446ed40c7ad0161fef245b2654fb2cc2c
dnyaneshmali <dnyanmali@gmail.com>
2016-11-14 18:25:58 +0530

    admin header footer

---
+++ application/views/admin/footer.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="<?php echo base_url().'/js/jquery-2.1.1.js' ?>"></script>
+<script type="text/javascript" src="<?php echo base_url().'/js/materialize.js' ?>"></script>
+<script type="text/javascript" src="<?php echo base_url().'/js/bootstrap.min.js' ?>"></script>
+<script type="text/javascript" src="<?php echo base_url().'/js/bootstrapValidator.js' ?>"></script>
+ </body>
+ </html>
--- application/views/admin/listevent.php
+++ application/views/admin/listevent.php
@@ -1,61 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title><?php echo $m_title ?></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <link href="../../css/adminmaterialize.css" type="text/css" rel="stylesheet" media="screen">
-        <link href="../../css/font-awesome.min.css" type="text/css" rel="stylesheet" media="screen">
-        <link href="../../css/admin.css" type="text/css" rel="stylesheet" media="screen">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    </head>
-    <body>
-               
-        <header id="header" class="page-topbar ">
-            <div class="navbar-fixed">
-                <nav class="navbar-color">
-                    <div class="nav-wrapper">
-                        <ul class="left">
-                            <li>
-                                <h1 class="logo-wrapper">
-                                    <a href="#" class="brand-logo darken-1" >
-                                        EthioEdu
-                                    </a>    
-                                </h1>
-                                <span class="logo-text">Ethioedu</span>
-                            </li>
-                        </ul>
-                        <ul class="right hid-mid-screen">
-                            <li>
-                                <a href="javascript:void(0);" class="waves-effect waves-block waves-light toggle-fullscreen">
-                                    <i class="mdi-action-settings-overscan"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0);" class="waves-effect waves-block waves-light notification-button" data-activates="notifications-dropdown">
-                                    <i class="mdi-social-notifications"><small class="notification-badge">5</small></i>
-
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" data-activates="chat-out" class="waves-effect waves-block waves-light chat-collapse">
-                                    <i class="mdi-communication-chat"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </nav>
-            </div>
-
-        </header>
-        <!--header End-->
-
+<?php  $this->load->view('admin/header'); ?>
         <!--start main-->
         <div id="main">
             <div class="wrapper">
@@ -126,16 +69,4 @@ and open the template in the editor.
                 </section>
             </div>
         </div>
-        <script type="text/javascript" src="../../js/jquery-2.1.1.js"></script>
-        <script type="text/javascript" src="../../js/materialize.js"></script>
-    </body>
-
-</html>
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+        <?php  $this->load->view('admin/footer'); ?>
