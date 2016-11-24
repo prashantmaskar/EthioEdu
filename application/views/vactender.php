97986ef0e073df56f208470145b1dfb6922e182e
dnyaneshmali <dnyanmali@gmail.com>
2016-11-24 15:33:22 +0530

    remove js files from vactender

--- application/views/vactender.php
+++ application/views/vactender.php
@@ -110,11 +110,3 @@
             </div>
         </div>
        <?php $this->load->view('footer'); ?>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pickadate.js/3.5.6/picker.js"></script>
-<script type="text/javascript" src="<?php echo base_url(). 'js/sorttender.js'?>"></script>
-<script type="text/javascript" src="<?php echo base_url().'js/dateValidation.js'?>"></script>
-<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.js"></script>
-</body>
-</html>
