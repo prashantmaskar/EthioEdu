ff54775169066e3653c50494d0da6a676264eb71
harshalghule <harshal.ghule@softinfology.com>
2016-12-02 18:13:22 +0530

    ansques

---
+++ application/views/like.php
@@ -0,0 +1,24 @@
+<?php 
+$id1 = $_POST['id1'];
+$u_id = $_POST['id2'];
+$res_id = $_POST['id3'];
+if(isset($id1)){
+	//echo $ques_id = $_POST['id1'];
+ //echo $u_id = $_POST['id2'];
+ 
+
+$query = $this->db->query("UPDATE tbl_userresponse set response_like= '0' where response_id= '".$res_id."' and user_id= '".$u_id."'");
+echo json_encode($query->result_array());
+}
+
+/*
+$ques_id = $_POST['ques_id'];
+ $u_id = $_POST['u_id'];
+ $res_id = $_POST['res_id'];
+if(isset($ques_id)){
+
+$query = $this->db->query("UPDATE tbl_userresponse set response_like = '0' where question_id = '" .$ques_id. "' and user_id = '" .$u_id. "' and response_id = '" .$res_id. "' ");
+}
+
+*/
+?>
