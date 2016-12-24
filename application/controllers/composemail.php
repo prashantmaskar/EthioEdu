08d0b47d751c38a40e28c93431d99246f0104898
Santosh <santosh.bhosale123@gmail.com>
2016-12-24 01:16:05 -0800

    conflict solved

--- application/controllers/composemail.php
+++ application/controllers/composemail.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class composemail extends CI_Controller {
+class Composemail extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -34,8 +34,8 @@ class composemail extends CI_Controller {
 		$related_res = $this->init_models->related_users($sessid);
 		$banners = $this->init_models->getadvertisebanners();
             $view_params = array(
-                'm_title' => 'Compose Mail',
-                'title'   => 'Compose Mail',
+                'm_title' => 'Studenet inbox',
+                'title'   => 'Studenet inbox',
                 'banners' => $banners,
                 'related_res' => $related_res,
             );
