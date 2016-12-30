3fe11acfdffc7b3bf457cff5aa168b426b435c3f
monica <monicamca3@gmail.com>
2016-12-30 16:30:57 +0530

    category count of course and questions

--- application/models/Init_models.php
+++ application/models/Init_models.php
@@ -32,8 +32,13 @@
         //dynamic added categories
       public function get_categories()  
       {  
-           $query = $this->db->query("SELECT * from tbl_category where category_type='course'");
+          // $query = $this->db->query("SELECT * from tbl_category where category_type='course'");
+            //$query = $this->db->query("SELECT tbl_category.*,tbl_course.course_category FROM tbl_category LEFT JOIN tbl_course ON tbl_category.category_id=tbl_course.course_category where category_type='course'");
+            $query = $this->db->query("SELECT tbl_category.*,tbl_course.course_category FROM tbl_category LEFT JOIN tbl_course ON tbl_category.category_id=tbl_course.course_category where category_type='course' GROUP BY tbl_course.course_category");
+         
+            
             return $query->result_array();
+
       }
        
        public function get_school_categories()  
@@ -43,7 +48,8 @@
       }
       public function get_Que_categories()  
       {  
-           $query = $this->db->query("SELECT * from tbl_category where category_type='question'");
+          // $query = $this->db->query("SELECT * from tbl_category where category_type='question'");
+         $query = $this->db->query("SELECT tbl_category.*,tbl_questions.question_category FROM tbl_category LEFT JOIN tbl_questions ON tbl_category.category_id=tbl_questions.question_category where category_type='question' GROUP BY tbl_questions.question_category");
             return $query->result_array();
       }
        public function get_project_categories()  
