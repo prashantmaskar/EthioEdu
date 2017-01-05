29d4bbf7d6016cd6e23ab71a6bba0d56b756a878
Santosh <santosh.bhosale123@gmail.com>
2017-01-04 23:59:42 -0800

    download pdf/word

--- application/views/Projectlist.php
+++ application/views/Projectlist.php
@@ -136,7 +136,7 @@
                                             <span class="project_title"><?php echo $row->project_title;?></span><br>
                                         </a>
                                 
-                                         <span class="project_sinfo">Course:<a href="#"><?php echo $row->project_course;?></a> Download: <a href="\ETHIO\uploads\Documents\<?php echo $row->project_upload;?>"><?php echo $row->project_upload;?> </a> Format: <strong class="green_text"><?php echo $row->project_format; ?></strong></span> Type:<a href="#">Project Topic</a> Price:<strong class="green_text">4000</strong></span>
+                                         <span class="project_sinfo">Course:<a href="#"><?php echo $row->project_course;?></a> Download: <a href="<?php echo base_url() . 'uploads/Documents/' .$row->project_upload;?>"><?php echo $row->project_upload;?> </a> Format: <strong class="green_text"><?php echo $row->project_format; ?></strong></span> Type:<a href="#">Project Topic</a> Price:<strong class="green_text">4000</strong></span>
                                     </li>
                                     <?php }}else{
 
