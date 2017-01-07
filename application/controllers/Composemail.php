baf7564ec4950fb1e46f73ffa42a0279b4a3a691
Santosh <santosh.bhosale123@gmail.com>
2017-01-06 17:47:30 -0800

    session to compose mail

--- application/controllers/Composemail.php
+++ application/controllers/Composemail.php
@@ -25,6 +25,12 @@ class Composemail extends CI_Controller {
         $this->load->library(array('session', 'form_validation', 'email'));
         $this->load->database();
         $this->load->model('init_models');
+    if ( !$this->session->userdata('logged_in'))
+    { 
+        $this->session->set_userdata('referred_from', current_url());
+        redirect('index.php/login');
+    }
+
     }
 
 
